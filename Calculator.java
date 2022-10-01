56c107a31ed2dd0ad9a8340fcfbf1f1fe7262ccd
ravi406 <ravikamble406@gmail.com>
2022-10-01 16:16:34 +0530

    added sub method

--- Calculator.java
+++ Calculator.java
@@ -4,9 +4,14 @@ public class Calculator {
     public static void add(int a, int b){
         System.out.println(a+b);
     }
+
+    public static void sub(int a, int b){
+        System.out.println(a-b);
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to calculator program");
         add(5,6);
+        sub(5,3);
 
     }
 }
