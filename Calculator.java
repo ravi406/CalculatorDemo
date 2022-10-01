02a27afba88bb0c48466eb67298ba0fcb01c65c0
ravi406 <ravikamble406@gmail.com>
2022-10-01 16:08:33 +0530

    added add method

--- Calculator.java
+++ Calculator.java
@@ -1,9 +1,12 @@
 package com.calc;
 
 public class Calculator {
+    public static void add(int a, int b){
+        System.out.println(a+b);
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to calculator program");
-
+        add(5,6);
 
     }
 }
