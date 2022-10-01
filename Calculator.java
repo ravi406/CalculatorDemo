88ddbc4eba29fdc7f2703c05b5bde4ba8308a994
ravi406 <ravikamble406@gmail.com>
2022-10-01 16:25:44 +0530

    added multiplication method

--- Calculator.java
+++ Calculator.java
@@ -8,10 +8,15 @@ public class Calculator {
     public static void sub(int a, int b){
         System.out.println(a-b);
     }
+
+    public static void mul(int a, int b){
+        System.out.println(a*b);
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to calculator program");
         add(5,6);
         sub(5,3);
+        mul(5,5);
 
     }
 }
